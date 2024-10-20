a5048d9a7ae30b42ae66f6ef4ddfcd355d249938
galXploit <gaxxstorezzx@gmail.com>
2024-10-20 08:41:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Button untuk menampilkan pesan alert
+document.getElementById('alertButton').addEventListener('click', () => {
+    alert('Hello! You clicked the button.');
+});
+
+// Button untuk mengarahkan ke Google
+document.getElementById('linkButton').addEventListener('click', () => {
+    window.location.href = 'https://www.google.com';
+});
